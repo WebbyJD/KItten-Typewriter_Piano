54e518a52a87b5ad3c170ccade91fa887340f807
tatabbyi <ytrachel0@gmail.com>
2026-04-02 19:10:27 +0100

    feat: Add Keys

--- src/java/ui/Keys.java
+++ src/java/ui/Keys.java
@@ -1,5 +1,23 @@
 package java.ui;
 
-public class Keys {
+public final class Keys {
+    public static final String ROW_TOP = "CDEFGABC";
+    public static final String ROW_MID = "CDEFGAB";
+    public static final String ROW_BOT = "#DEFGABb";
     
+    private Keys() {}
+        public static String row(int index) {
+            return switch (index) {
+                case 0 -> ROW_TOP;
+                case 1 -> ROW_MID;
+                case 2 -> ROW_BOT;
+                default -> throw new IndexOutOfBoundsException();
+            };
+        }
+    
+    public static int rowLength(int index){
+        return row(index).length();
+    }
 }
+    
+
